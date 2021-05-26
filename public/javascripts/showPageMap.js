d8003c17f0988fb436e6830f66f0b12864125802
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-26 05:05:47 -0400

    customize map popup and map style

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,9 +3,16 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: campground.geometry.coordinates,
-    zoom: 8
+    zoom: 8,
+    style: 'mapbox://styles/mapbox/light-v10'
   });
 
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({offset: 25})
+    .setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
   .addTo(map)

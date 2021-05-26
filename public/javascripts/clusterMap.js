58e198eccdc828054d1bcd40c2aa7c6e7ad82bcf
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-26 15:26:16 -0400

    handled clustermap now displays campgrounds locations accurately

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,5 +1,4 @@
-
-	mapboxgl.accessToken = mapToken;
+mapboxgl.accessToken = mapToken;
 var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/dark-v10',
@@ -15,7 +14,7 @@ map.addSource('earthquakes', {
 type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+data: campgrounds,
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)

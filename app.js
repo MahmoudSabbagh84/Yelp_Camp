adc4027c12c9a928685801d2e32a5c748c00d694
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-27 04:34:30 -0400

    appjs edits

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-console.log(process.env.SECRET);
-console.log(process.env.API_KEY);
-
 const express = require('express');
 const path = require('path');
-const engine = require('ejs-mate');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const session = require('express-session');
-const flash = require('connect-flash')
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
@@ -18,55 +15,53 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-
-
-const userRoutes = require('./routes/users')
+const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const { initialize } = require('passport');
-const MongoStore = require('connect-mongo');
+
+const MongoDBStore = require("connect-mongo")(session);
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
 mongoose.connect(dbUrl, {
-    useNewUrlParser: true, 
-    useCreateIndex: true, 
+    useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-})
+});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open' , () =>{
-    console.log('Database Connected')
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
 const app = express();
 
-app.engine('ejs', engine)
-app.set('view engine' , 'ejs');
-app.set('views' , path.join(__dirname,'views'));
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize({
     replaceWith: '_'
-}));
+}))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
-
-const store = new MongoStore({
-    mongoUrl: dbUrl,
-    secret ,
-    touchAfter: 24 * 60 * 60 
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
 });
 
-store.on('error' , function(e){
-    console.log('SESSION STORE ERROR' , e)
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
 })
 
 const sessionConfig = {
-    store ,
+    store,
     name: 'session',
     secret,
     resave: false,
@@ -75,11 +70,15 @@ const sessionConfig = {
         httpOnly: true,
         // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge : 1000 * 60 * 60 * 24 * 7
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig))
+
+app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+
 const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
     "https://api.tiles.mapbox.com/",
@@ -95,7 +94,6 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net/"
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
@@ -117,7 +115,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/dxbouoiqv/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
@@ -141,24 +139,27 @@ app.use((req, res, next) => {
 })
 
 
-app.use('/',userRoutes);
-app.use('/campgrounds',campgroundRoutes);
-app.use('/campgrounds/:id/reviews' , reviewRoutes)
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
-app.get('/' , (req, res)=>{
+
+app.get('/', (req, res) => {
     res.render('home')
+});
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
-app.all('*' , (req,res,next)=>{
-    next(new ExpressError('PAGE NOT FOUND' , 404))
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
-app.use((err,req,res,next)=>{
-    const {statusCode = 500 } = err;
-    if(!err.message) err.message = 'Oh No, Something went wrong!'
-    res.status(statusCode).render('error',{err})
-})
-const port = process.env.PORT || 3000
-app.listen(port, () =>{
-    console.log(`SERVING ON PORT ${port}!!!`)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })

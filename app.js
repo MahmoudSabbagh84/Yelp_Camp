695c0c52b41de4a5fae13db1126e98577d9d0fc8
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-27 02:00:52 -0400

    setup and migrate to mongo atlas

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { initialize } = require('passport');
- 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL;
+// 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
     useNewUrlParser: true, 
     useCreateIndex: true, 
     useUnifiedTopology: true,

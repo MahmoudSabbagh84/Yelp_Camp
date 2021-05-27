1df7fad25925ccc87ad0f9fb5b7f5f5fb27f52f3
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-27 04:33:06 -0400

    appjs edits

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { initialize } = require('passport');
 const MongoStore = require('connect-mongo');
-// const dbUrl = process.env.DB_URL;
-// 'mongodb://localhost:27017/yelp-camp'
+
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true, 

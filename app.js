e31317edf507d34de38386b96e4bbcf4f768a5d4
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-27 02:44:17 -0400

    handles session storing on mongodb/reverted to local db to handle this fix

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { initialize } = require('passport');
-const dbUrl = process.env.DB_URL;
+const MongoStore = require('connect-mongo');
+// const dbUrl = process.env.DB_URL;
 // 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true, 
     useCreateIndex: true, 
@@ -52,7 +54,18 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }));
 
+const store = new MongoStore({
+    mongoUrl: dbUrl,
+    secret: 'thisshouldbeabettersecret!',
+    touchAfter: 24 * 60 * 60
+});
+
+store.on('error' , function(e){
+    console.log('SESSION STORE ERROR' , e)
+})
+
 const sessionConfig = {
+    store ,
     name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,

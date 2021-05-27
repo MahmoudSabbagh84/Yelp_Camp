99e1346fd583656f20c9cc2257f78e086b696b28
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2021-05-27 03:36:37 -0400

    heroku prep

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const { initialize } = require('passport');
 const MongoStore = require('connect-mongo');
 // const dbUrl = process.env.DB_URL;
 // 'mongodb://localhost:27017/yelp-camp'
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true, 
     useCreateIndex: true, 
@@ -54,10 +54,12 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
+
 const store = new MongoStore({
     mongoUrl: dbUrl,
-    secret: 'thisshouldbeabettersecret!',
-    touchAfter: 24 * 60 * 60
+    secret ,
+    touchAfter: 24 * 60 * 60 
 });
 
 store.on('error' , function(e){
@@ -67,7 +69,7 @@ store.on('error' , function(e){
 const sessionConfig = {
     store ,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
